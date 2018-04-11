1066c83c2e18ba41680d7a2e3578192d7b88bd3b
im-minion <vaibhavminiyar@gmail.com>
2018-04-11 16:21:19 +0530

    handled blog clicking

--- lib/BlogRow.dart
+++ lib/BlogRow.dart
@@ -16,59 +16,66 @@ class BlogRow extends StatelessWidget {
 //    final double itemHeight = (size.height - kToolbarHeight - 24) / 2.2;
 //    final double itemWidth = size.width;
     return new Container(
-      child: new Card(
-        elevation: 2.5,
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            new FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              image: snapshot.value['IMAGE'],
-              alignment: Alignment.topCenter,
-              fit: BoxFit.contain,
-            ),
+      child: new InkWell(
+        child: new Card(
+          elevation: 2.5,
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: snapshot.value['IMAGE'],
+                alignment: Alignment.topCenter,
+                fit: BoxFit.contain,
+              ),
 //            new Image.network(snapshot.value['IMAGE']),
-            new Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 0.0),
-              child: new Text(
-                snapshot.value['Title'],
-                style:
-                    new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              new Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 0.0),
+                child: new Text(
+                  snapshot.value['Title'],
+                  style:
+                      new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
               ),
-            ),
-            new Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 0.0),
-              child: new Text(
-                snapshot.value['DESCRIPTION'],
-                style: new TextStyle(
-                    fontWeight: FontWeight.normal, fontSize: 16.0),
+              new Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 0.0),
+                child: new Text(
+                  snapshot.value['DESCRIPTION'],
+                  style: new TextStyle(
+                      fontWeight: FontWeight.normal, fontSize: 16.0),
+                ),
               ),
-            ),
-            new Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                new Text(snapshot.value['username'],
-                    style: new TextStyle(color: Colors.blueAccent)),
-                new IconButton(
-                    icon: _liked
-                        ? new Icon(
-                            Icons.favorite,
-                            color: Colors.blueAccent,
-                          )
-                        : new Icon(
-                            Icons.favorite_border,
-                            color: Colors.blueAccent,
-                          ),
-                    onPressed: () {
-                      Scaffold.of(context).showSnackBar(new SnackBar(
-                            content: new Text("Coming Soon!"),
-                          ));
-                    }),
-              ],
-            ),
-          ],
+              new Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  new Text(snapshot.value['username'],
+                      style: new TextStyle(color: Colors.blueAccent)),
+                  new IconButton(
+                      icon: _liked
+                          ? new Icon(
+                              Icons.favorite,
+                              color: Colors.blueAccent,
+                            )
+                          : new Icon(
+                              Icons.favorite_border,
+                              color: Colors.blueAccent,
+                            ),
+                      onPressed: () {
+                        Scaffold.of(context).showSnackBar(new SnackBar(
+                              content: new Text("Coming Soon!"),
+                            ));
+                      }),
+                ],
+              ),
+            ],
+          ),
         ),
+        onTap: (){
+          Scaffold.of(context).showSnackBar(new SnackBar(
+            content: new Text("Clicked on Blog "+snapshot.value['Title']),
+          ));
+        },
       ),
     );
   }
